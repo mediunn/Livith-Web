2a1309826dd531b73a5270dcb98b1b69f884e80c
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-14 19:32:17 +0900

    style : 디자인 시스템 폰트 적용 테스트

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,15 @@ function App() {
       <p className="text-grayScaleBlack30">Livith</p>
       <p className="text-grayScaleBlack5">Livith</p>
       <p className="text-grayScaleWhite">Livith</p>
+
+      <p className="text-head-lg font-semibold font-NotoSansKR">Livith</p>
+      <p className="text-head-sm font-normal font-NotoSansKR">Livith</p>
+      <p className="text-body-lg font-semibold font-NotoSansKR">Livith</p>
+      <p className="text-body-md font-medium font-NotoSansKR">Livith</p>
+      <p className="text-body-sm font-normal font-NotoSansKR">Livith</p>
+      <p className="text-caption-lg font-semibold font-NotoSansKR">Livith</p>
+      <p className="text-caption-sm font-normal font-NotoSansKR">Livith</p>
+      <p className="text-caption-ssm font-normal font-NotoSansKR">Livith</p>
     </>
   );
 }
