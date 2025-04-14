39a7fcb79b9e69d705734afb89bd690d874040d8
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-14 18:36:03 +0900

    style : 디자인 시스템 컬러 적용 테스트

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-mainYellow30">Livith</p>
+      <p className="text-mainYellow60">Livith</p>
+      <p className="text-caution100">Livith</p>
+      <p className="text-grayScaleBlack100">Livith</p>
+      <p className="text-grayScaleBlack90">Livith</p>
+      <p className="text-grayScaleBlack80">Livith</p>
+      <p className="text-grayScaleBlack50">Livith</p>
+      <p className="text-grayScaleBlack30">Livith</p>
+      <p className="text-grayScaleBlack5">Livith</p>
+      <p className="text-grayScaleWhite">Livith</p>
     </>
   );
 }
