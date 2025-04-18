c898bb2633a9769e7ea377b532108b8e0158430c
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-18 18:20:38 +0900

    stlye : 아이콘 버튼화

--- src/shared/ui/TabBar.tsx
+++ src/shared/ui/TabBar.tsx
@@ -6,8 +6,12 @@ function TabBar() {
     <div className="absolute bottom-0 w-375 h-64 ">
       <div className="w-375 h-1 bg-grayScaleBlack80"></div>
       <div className="flex justify-between">
-        <img src={HomeIcon} alt="home" className="pl-92 pt-13 w-38 h-38" />
-        <img src={MyIcon} alt="my" className="pr-92 pt-13 w-38 h-38" />
+        <button className="w-38 h-38 mt-13 ml-92 p-0 bg-transparent border-none cursor-pointer">
+          <img src={HomeIcon} alt="home" className="w-full h-full" />
+        </button>
+        <button className="w-38 h-38 mt-13 mr-92 p-0 bg-transparent border-none cursor-pointer">
+          <img src={MyIcon} alt="my" className="w-full h-full" />
+        </button>
       </div>
     </div>
   );
