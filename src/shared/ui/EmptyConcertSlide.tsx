5b24c4584e2131c2ba69a30270ffe1715a525c51
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-21 22:08:31 +0900

    style : 콘서트 슬라이드 엠티뷰 컴포넌트 분리

---
+++ src/shared/ui/EmptyConcertSlide.tsx
@@ -0,0 +1,27 @@
+import EmptyIcon from "../assets/EmptyIcon.svg";
+
+type EmptyConcertSlideProps = {
+  status: "prev" | "current" | "next";
+};
+
+function EmptyConcertSlide({ status }: EmptyConcertSlideProps) {
+  const statusText =
+    status === "prev"
+      ? "한 달 이내 진행했던"
+      : status === "current"
+        ? "현재 진행하는"
+        : "곧 진행하는";
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-90 mb-90">
+      <img src={EmptyIcon} className="w-50 h-40" />
+      <p className="text-grayScaleBlack80 text-body-md font-medium font-NotoSansKR text-center mt-16 mb-0">
+        {statusText}
+        <br />
+        콘서트가 없어요
+      </p>
+    </div>
+  );
+}
+
+export default EmptyConcertSlide;
--- src/widgets/NextConcert.tsx
+++ src/widgets/NextConcert.tsx
@@ -1,4 +1,4 @@
-import EmptyIcon from "../shared/assets/EmptyIcon.svg";
+import EmptyConcertSlide from "../shared/ui/EmptyConcertSlide";
 
 function NextConcert() {
   return (
@@ -8,14 +8,7 @@ function NextConcert() {
           곧 진행하는 콘서트
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center mt-90 mb-90">
-        <img src={EmptyIcon} className="w-50 h-40" />
-        <p className="text-grayScaleBlack80 text-body-md font-medium font-NotoSansKR text-center mt-16 mb-0">
-          곧 진행하는
-          <br />
-          콘서트가 없어요
-        </p>
-      </div>
+      <EmptyConcertSlide status="next" />
     </div>
   );
 }
