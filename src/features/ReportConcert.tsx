c2d86a49011382431274fc10c2ecb1c632a621ac
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-30 16:45:20 +0900

    feat : 제보 버튼 클릭 시 페이지 이동

--- src/features/ReportConcert.tsx
+++ src/features/ReportConcert.tsx
@@ -1,4 +1,8 @@
 function ReprotConcert() {
+  const handleClick = () => {
+    window.location.href = "https://forms.gle/q7uQEr4XSiQmoVkM6";
+  };
+
   return (
     <>
       <div className="flex justify-between">
@@ -6,7 +10,10 @@ function ReprotConcert() {
           더 빛나는 콘서트를 <br />
           위한 정보를 알고 있다면
         </p>
-        <button className="mr-16 mt-172 w-130 h-44 bg-mainYellow30 rounded-30 text-grayScaleBlack100 text-body-md font-medium font-NotoSansKR ">
+        <button
+          onClick={handleClick}
+          className="mr-16 mt-172 w-130 h-44 bg-mainYellow30 rounded-30 text-grayScaleBlack100 text-body-md font-medium font-NotoSansKR "
+        >
           콘서트 제보하기
         </button>
       </div>
