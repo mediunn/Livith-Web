86d84e98a43592a03195bc3ed682bf7b10ccb75d
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-04-30 16:49:25 +0900

    feat : 업데이트 노트 버튼 클릭 시 페이지 이동

--- src/features/Info.tsx
+++ src/features/Info.tsx
@@ -1,6 +1,11 @@
 import MyNextArrow from "../shared/assets/MyNextArrow.svg";
 
 function Info() {
+  const handleClickUpdate = () => {
+    window.location.href =
+      "https://youz2me.notion.site/Livith-v-25-04-13-1d402dd0e5fc800dab7fc177f325eade?pvs=4";
+  };
+
   return (
     <div>
       <div className="pl-16 pr-18">
@@ -16,7 +21,10 @@ function Info() {
           <p className="m-0 text-grayScaleBlack30 text-body-sm font-regular font-NotoSansKR">
             업데이트 노트
           </p>
-          <button className="w-8 h-16 p-0 bg-transparent border-none cursor-pointer">
+          <button
+            onClick={handleClickUpdate}
+            className="w-8 h-16 p-0 bg-transparent border-none cursor-pointer"
+          >
             <img
               src={MyNextArrow}
               alt="next arrow"
