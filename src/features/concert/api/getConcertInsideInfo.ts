e9096033158287e2ec6f5bf3a1c655a975433f0d
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-05-09 01:11:43 +0900

    feat : 특정 콘서트 상세 조회 API 연결

---
+++ src/features/concert/api/getConcertInsideInfo.ts
@@ -0,0 +1,10 @@
+import { ApiResponse } from "../../../shared/types/response";
+import axiosInstance from "../../../shared/api/axiosInstance";
+import { Concert } from "../../../entities/concert/types/index"; // index.ts에서 export된 타입 사용
+
+export async function getConcertInsideInfo(id: number): Promise<Concert> {
+  const response = await axiosInstance.get<ApiResponse<Concert>>(
+    `/concerts/${id}`
+  );
+  return response.data.data;
+}
