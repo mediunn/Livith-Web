9265d5229cffad99321996fee27b706b681d0e72
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-05-09 19:55:26 +0900

    feat : 특정 콘서트 문화 조회 API 연결

--- src/features/concert/ui/ConcertCultureCarousel.tsx
+++ src/features/concert/ui/ConcertCultureCarousel.tsx
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ConcertCultureCarouselSlide from "../shared/ui/ConcertCultureCarouselSlide";
-import "../shared/styles/slick-theme.css";
-import PrevArrow from "../shared/assets/PrevArrow.svg";
-import NextArrow from "../shared/assets/NextArrow.svg";
+import ConcertCultureCarouselSlide from "../../../shared/ui/ConcertCultureCarouselSlide";
+import "../../../shared/styles/slick-theme.css";
+import PrevArrow from "../../../shared/assets/PrevArrow.svg";
+import NextArrow from "../../../shared/assets/NextArrow.svg";
+import { getConcertCulture } from "../api/getConcertCulture";
 
-function ConcertCultureCarousel() {
+type ConcertCultureProps = {
+  id: number;
+  concertId: number;
+  content: string;
+  imgUrl: string;
+};
+
+type ConcertCultureCarouselProps = {
+  concertId: number;
+};
+
+function ConcertCultureCarousel({ concertId }: ConcertCultureCarouselProps) {
   // pc일 경우 마우스 hover시 캐러셀 전환 버튼 나타나도록
   const [isHovered, setIsHovered] = useState(false);
+
+  const [ConcertCulture, setConcertCulture] = useState<ConcertCultureProps[]>(
+    []
+  );
+
+  useEffect(() => {
+    const fetchConcertCulture = async () => {
+      try {
+        const data = await getConcertCulture(concertId);
+        setConcertCulture(data);
+      } catch (error) {
+        console.error("특정 콘서트 문화 조회 API 호출 실패:", error);
+      }
+    };
+
+    fetchConcertCulture();
+  }, [concertId]);
 
   const CustomPrevArrow = (props: any) => {
     const { onClick, style } = props;
@@ -60,11 +89,11 @@ function ConcertCultureCarousel() {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Slider {...settings}>
-        {slideData.map((slide, index) => (
+        {ConcertCulture.map((slide) => (
           <ConcertCultureCarouselSlide
-            key={index}
-            text={slide.text}
-            imageUrl={slide.imageUrl}
+            key={slide.id}
+            content={slide.content}
+            imageUrl={slide.imgUrl}
           />
         ))}
       </Slider>
@@ -73,21 +102,3 @@ function ConcertCultureCarousel() {
 }
 
 export default ConcertCultureCarousel;
-
-const slideData = [
-  {
-    text: '앵콜 무대 전에 각자 준비해온 "손수건"을 던져야 해요',
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPfm09-_Zpps0psShmq7VMODDvga6tOcEwNw&s",
-  },
-  {
-    text: '앵콜 무대 전에 "응원봉"을 들고 "파도타기"를 해요',
-    imageUrl:
-      "https://img.seoul.co.kr/img/upload/2019/10/30/SSI_20191030200936.jpg",
-  },
-  {
-    text: '직접 제작한 "비즈팔찌"를 팬들과 교환해요',
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pUTNDB4G3kQKU5QWo7LD3nCkH1AgCoqnrg&s",
-  },
-];
--- src/widgets/ConcertCulture.tsx
+++ src/widgets/ConcertCulture.tsx
@@ -1,13 +1,16 @@
-import ConcertCultureCarousel from "../features/ConcertCultureCarousel";
+import { useParams } from "react-router-dom";
+import ConcertCultureCarousel from "../features/concert/ui/ConcertCultureCarousel";
 import EmptyConcertCulture from "../shared/ui/EmptyConcertCulture";
 
 function ConcertCulture() {
+  const { concertId } = useParams<{ concertId: string }>();
+
   return (
     <div className="w-375 ml-16">
       <p className="text-grayScaleWhite text-body-lg font-semibold font-NotoSansKR mt-30 mb-14">
         해당 공연 문화
       </p>
-      <ConcertCultureCarousel />
+      <ConcertCultureCarousel concertId={Number(concertId)} />
       {/* <EmptyConcertCulture /> */}
     </div>
   );
