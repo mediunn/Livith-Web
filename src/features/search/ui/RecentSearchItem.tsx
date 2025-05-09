0569075b9f7f555dd6bd6d13328de9e2a5a7649f
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-09 17:35:28 +0900

    feat: 최근 검색어 로컬 스토리지 연결

--- src/features/search/ui/RecentSearchItem.tsx
+++ src/features/search/ui/RecentSearchItem.tsx
@@ -1,11 +1,31 @@
 import CloseIcon from "../../../shared/assets/CloseIcon.svg";
-function RecentSearchItem({ word }: { word: string }) {
+import { StateWithSetter } from "../../../shared/types/props";
+
+interface RecentSearchItemProps {
+  word: string;
+  recentState: StateWithSetter<string[]>;
+}
+
+const STORAGE_KEY = "recentSearches";
+
+function RecentSearchItem({ word, recentState }: RecentSearchItemProps) {
+  const { value: recent, setValue: setRecent } = recentState;
+  const handleDeleteRecent = () => {
+    const updated = recent.filter((item) => item !== word);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+    setRecent(updated);
+  };
+
   return (
-    <div className="w-fit border-1 border-solid border-grayScaleBlack30 rounded-26 flex items-center px-10 py-3">
+    <div
+      className="w-fit border-1 border-solid border-grayScaleBlack30 rounded-26 flex items-center px-10 py-3"
+      // 클릭 이벤트 추가 예정
+      // onClick={() => console.log(word)}
+    >
       <p className="text-grayScaleBlack30 text-body-sm font-regular font-NotoSansKR mr-4 ">
         {word}
       </p>
-      <img src={CloseIcon} alt="삭제 아이콘" />
+      <img src={CloseIcon} alt="삭제 아이콘" onClick={handleDeleteRecent} />
     </div>
   );
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import InputSearchBar from "../features/search/ui/InputSearchBar";
 import RecentSearch from "../features/search/ui/RecentSearch";
 
 function SearchPage() {
+  const [input, setInput] = useState<string>("");
+  const [recent, setRecent] = useState<string[]>([]);
+  const STORAGE_KEY = "recentSearches";
+
+  // 처음 렌더링 시 localStorage에서 불러오기
+  useEffect(() => {
+    const recentSearch = localStorage.getItem(STORAGE_KEY);
+    if (recentSearch) {
+      setRecent(JSON.parse(recentSearch));
+    }
+  }, []);
   return (
     <>
-      <InputSearchBar />
-      <RecentSearch />
+      <InputSearchBar
+        inputState={{ value: input, setValue: setInput }}
+        recentState={{ value: recent, setValue: setRecent }}
+      />
+
+      {!input && recent.length > 0 && (
+        <RecentSearch value={recent} setValue={setRecent} />
+      )}
     </>
   );
 }
