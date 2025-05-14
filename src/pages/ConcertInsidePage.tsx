b1d588965b9848c86c216fa91578e71dc5cf17f6
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-14 17:15:29 +0900

    feat: 콘서트 상태에 따라 셋리스트 종류 다르게 설정

--- src/pages/ConcertInsidePage.tsx
+++ src/pages/ConcertInsidePage.tsx
@@ -1,21 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import SearchBar from "../shared/ui/SearchBar";
 import ConcertInsideInfo from "../features/concert/ui/ConcertInsideInfo";
 import ConcertCulture from "../widgets/ConcertCulture";
-import SetList from "../widgets/SetList";
+import PastSetList from "../shared/ui/PastSetList";
 import ExpectationSetList from "../shared/ui/ExpectationSetList";
+import { ConcertStatus } from "../entities/concert/types";
+import OngoingSetList from "../shared/ui/OngoingSetlist";
 
 function ConcertInsidePage() {
   const { concertId } = useParams<{ concertId: string }>();
+  const location = useLocation();
+  const status = location.state?.status;
 
   return (
     <>
       <SearchBar hideLogo></SearchBar>
-
       <ConcertInsideInfo concertId={Number(concertId)}></ConcertInsideInfo>
       <ConcertCulture></ConcertCulture>
-      <ExpectationSetList></ExpectationSetList>
-      <SetList></SetList>
+      {status === ConcertStatus.UPCOMING ? (
+        <>
+          <ExpectationSetList></ExpectationSetList>
+          <PastSetList></PastSetList>
+        </>
+      ) : (
+        <OngoingSetList></OngoingSetList>
+      )}
     </>
   );
 }
---
+++ src/shared/ui/PastSetList.tsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import ConcertRightArrow from "../assets/ConcertRightArrow.svg";
+import SetListCard from "./SetListCard";
+import { SetlistType } from "../../entities/setlist/types";
+
+function PastSetList() {
+  const navigate = useNavigate();
+  const concertId = 1; // 추후 API 연동 시 데이터로 대체
+  return (
+    <div>
+      <div className="flex item-center justify-between w-full">
+        <p className="text-grayScaleWhite text-body-lg font-semibold font-NotoSansKR mt-25 mb-19 ml-16">
+          지난 셋리스트
+        </p>
+        <button className="w-24 h-24 bg-transparent border-none p-0 mt-24 mr-16 cursor-pointer">
+          <img
+            src={ConcertRightArrow}
+            className="w-full h-full"
+            onClick={() =>
+              navigate(`/setlists/${SetlistType.PAST}/${concertId}`)
+            }
+          />
+        </button>
+      </div>
+      <div className="ml-16 flex gap-9 pb-92">
+        {/* 추후 API 연동 시 데이터로 대체 */}
+        {/* <SetListCard />
+        <SetListCard />
+        <SetListCard />
+        <SetListCard /> */}
+      </div>
+    </div>
+  );
+}
+
+export default PastSetList;
