63e66c914a579fb0a18d4a644e8724d451684f2b
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-25 22:15:04 +0900

    fix: 셋리스트 페이지 진입 시 스크롤 맨 위로 이동

--- src/pages/SetlistDetailPage.tsx
+++ src/pages/SetlistDetailPage.tsx
@@ -2,9 +2,15 @@ import { useParams } from "react-router-dom";
 import SetlistDetail from "../features/setlist/ui/SetlistDetail";
 import ListHeader from "../shared/ui/ListHeader";
 import SetlistSongList from "../features/setlist/ui/SetlistSongList";
+import { useEffect } from "react";
 
 function SetlistDetailPage() {
   const { setlistId, concertId } = useParams();
+  // 페이지 진입 시 스크롤 맨 위로 이동
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <ListHeader />
