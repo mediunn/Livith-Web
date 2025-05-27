4d6311fc9926d59ed4915be165f3eb512508b264
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-05-27 21:43:53 +0900

    stlye : 메인 이미지 캐러셀 배너 칩 크기 수정

--- src/shared/ui/MainImageCarouselSlide.tsx
+++ src/shared/ui/MainImageCarouselSlide.tsx
@@ -13,8 +13,8 @@ function MainImageCarouselSlide({
     <div className="relative w-full h-365">
       <div className="h-365 absolute inset-0 bg-gradient-to-t from-grayScaleBlack100 to-transparent opacity-50"></div>
       <img src={imageUrl} alt={title} className="w-full h-full object-cover" />
-      <div className="absolute top-214 left-16 inline-flex items-center justify-center h-14 px-10 py-10 rounded-12 bg-grayScaleBlack90">
-        <p className="text-grayScaleWhite text-caption-lg font-semibold font-NotoSansKR m-0">
+      <div className="absolute top-214 left-16 inline-flex items-center justify-cente px-13 py-8 rounded-24 bg-grayScaleBlack90">
+        <p className="text-grayScaleWhite text-caption-lg font-semibold font-NotoSansKR mz-0">
           {category}
         </p>
       </div>
