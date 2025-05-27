90f7d3e38d4f965531d5cd39d74d7c92dc3be10b
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-05-28 01:57:46 +0900

    style : 메인 이미지 캐러셀 화살표 버튼 위치 수정

--- src/features/concert/ui/MainImageCarousel.tsx
+++ src/features/concert/ui/MainImageCarousel.tsx
@@ -42,7 +42,7 @@ function MainImageCarousel() {
         className={`absolute left-16 top-111 w-38 h-38 z-10 bg-transparent border-none p-0 cursor-pointer
           ${isHovered ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
       >
-        <img src={PrevArrow} alt="prev" className="w-10 h-19" />
+        <img src={PrevArrow} alt="prev" className="w-10 h-19 ml-14" />
       </button>
     );
   };
@@ -56,7 +56,7 @@ function MainImageCarousel() {
         className={`absolute right-16 top-111 w-38 h-38 z-10 bg-transparent border-none p-0 cursor-pointer
           ${isHovered ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
       >
-        <img src={NextArrow} alt="next" className="w-10 h-19" />
+        <img src={NextArrow} alt="next" className="w-10 h-19 ml-14" />
       </button>
     );
   };
