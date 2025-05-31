63d057a31f6e8fa304c64e60e791e1669311bcf3
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-05-31 23:14:27 +0900

    style : 가사 오른쪽 여백 수정

--- src/features/lyric/ui/Lyric.tsx
+++ src/features/lyric/ui/Lyric.tsx
@@ -51,7 +51,7 @@ function Lyric({ songId, activeButtons, fanchantData }: LyricProps) {
   }
 
   return (
-    <div className="ml-16 pr-16 mt-76 w-full">
+    <div className="ml-16 pr-32 mt-76 w-full">
       {songData.lyrics.map((_, index) => (
         <div key={index} className="mb-44 w-full">
           {activeButtons[3] && setlistId !== null && fanchantData ? (
