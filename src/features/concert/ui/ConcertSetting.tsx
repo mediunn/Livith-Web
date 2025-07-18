7b48958436a6cc3c7e2cf2e2b2e20483d7ed9fe3
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-07-19 04:58:18 +0900

    style : 웹사이트 버튼 UI

--- src/features/concert/ui/ConcertSetting.tsx
+++ src/features/concert/ui/ConcertSetting.tsx
@@ -1,4 +1,6 @@
 import ConcertTicketArrowIcon from "../../../shared/assets/ConcertTicketArrowIcon.svg";
+import WebSiteEarthIcon from "../../../shared/assets/WebSiteEarthIcon.svg";
+import WebSiteArrowIcon from "../../../shared/assets/WebSiteArrowIcon.svg";
 
 function ConcertSetting() {
   return (
@@ -13,60 +15,76 @@ function ConcertSetting() {
       </div>
 
       <div className="w-full h-539 flex justify-center bg-grayScaleBlack90">
-        <div className="relative w-327 h-438 mt-24">
-          {/* 배경 이미지 + 마스크 */}
-          <div
-            className="w-full h-full"
-            style={{
-              WebkitMaskImage: "url('/ConcertTicketMask.svg')",
-              WebkitMaskRepeat: "no-repeat",
-              WebkitMaskSize: "cover",
-              maskImage: "url('/ConcertTicketMask.svg')",
-              maskRepeat: "no-repeat",
-              maskSize: "cover",
-              backgroundImage:
-                "url('https://cdn.imweb.me/thumbnail/20250530/bafd11a557308.jpg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          ></div>
-          {/* 그라데이션 + 마스크 */}
-          <div
-            className="absolute inset-0 bg-gradient-to-t from-grayScaleBlack100 to-transparent pointer-events-none"
-            style={{
-              WebkitMaskImage: "url('/ConcertTicketMask.svg')",
-              WebkitMaskRepeat: "no-repeat",
-              WebkitMaskSize: "cover",
-              maskImage: "url('/ConcertTicketMask.svg')",
-              maskRepeat: "no-repeat",
-              maskSize: "cover",
-            }}
-          />
+        <div>
+          <div className="relative w-327 h-438 mt-24">
+            {/* 배경 이미지 + 마스크 */}
+            <div
+              className="w-full h-full"
+              style={{
+                WebkitMaskImage: "url('/ConcertTicketMask.svg')",
+                WebkitMaskRepeat: "no-repeat",
+                WebkitMaskSize: "cover",
+                maskImage: "url('/ConcertTicketMask.svg')",
+                maskRepeat: "no-repeat",
+                maskSize: "cover",
+                backgroundImage:
+                  "url('https://cdn.imweb.me/thumbnail/20250530/bafd11a557308.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            {/* 그라데이션 + 마스크 */}
+            <div
+              className="absolute inset-0 bg-gradient-to-t from-grayScaleBlack100 to-transparent pointer-events-none"
+              style={{
+                WebkitMaskImage: "url('/ConcertTicketMask.svg')",
+                WebkitMaskRepeat: "no-repeat",
+                WebkitMaskSize: "cover",
+                maskImage: "url('/ConcertTicketMask.svg')",
+                maskRepeat: "no-repeat",
+                maskSize: "cover",
+              }}
+            />
 
-          <div className="absolute bottom-26 left-19">
-            <p className="text-grayScaleWhite text-head-lg font-semibold font-NotoSansKR">
-              호시노 겐 콘서트
-            </p>
-            <p className="pt-2 text-grayScaleWhite text-head-lg font-semibold font-NotoSansKR">
-              <span className="text-mainYellow30">두 달</span> 앞으로
-              다가왔어요!
-            </p>
+            <div className="absolute bottom-26 left-19">
+              <p className="text-grayScaleWhite text-head-lg font-semibold font-NotoSansKR">
+                호시노 겐 콘서트
+              </p>
+              <p className="pt-2 text-grayScaleWhite text-head-lg font-semibold font-NotoSansKR">
+                <span className="text-mainYellow30">두 달</span> 앞으로
+                다가왔어요!
+              </p>
 
-            <div className="pt-18 w-270 border-b border-dashed border-grayScaleBlack50 opacity-50" />
+              <div className="pt-18 w-270 border-b border-dashed border-grayScaleBlack50 opacity-50" />
 
-            <p className="pt-18 text-grayScaleBlack30 text-body-lgs font-regular font-NotoSansKR">
-              2025.09.13 ~ 2025.09.14
-            </p>
-            <p className="pt-4 text-grayScaleBlack30 text-body-lgs font-regular font-NotoSansKR">
-              올림픽공원 올림픽홀
-            </p>
+              <p className="pt-18 text-grayScaleBlack30 text-body-lgs font-regular font-NotoSansKR">
+                2025.09.13 ~ 2025.09.14
+              </p>
+              <p className="pt-4 text-grayScaleBlack30 text-body-lgs font-regular font-NotoSansKR">
+                올림픽공원 올림픽홀
+              </p>
+            </div>
+
+            <button className="absolute bottom-26 right-16 w-46 h-46 bg-transparent border-none cursor-pointer">
+              <img
+                src={ConcertTicketArrowIcon}
+                alt="concert ticket arrow"
+                className="w-full h-full"
+              />
+            </button>
           </div>
 
-          <button className="absolute bottom-26 right-16 w-46 h-46 bg-transparent border-none cursor-pointer">
+          <button className="w-327 h-37 mt-16 flex items-center justify-center gap-4 text-grayScaleBlack100 text-body-sm font-semibold font-NotoSansKR bg-mainYellow30 rounded-6 border-none cursor-pointer">
             <img
-              src={ConcertTicketArrowIcon}
-              alt="concert ticket arrow"
-              className="w-full h-full"
+              src={WebSiteEarthIcon}
+              alt="web site earth"
+              className="w-18 h-18"
+            />
+            Gen Hoshino presents MAD HOPE Asia Tour ...
+            <img
+              src={WebSiteArrowIcon}
+              alt="web site arrow"
+              className="w-8 h-8"
             />
           </button>
         </div>
