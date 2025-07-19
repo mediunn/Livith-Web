e83497c428f33b8a806e40fbd0b7fad1c7bc30b2
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-07-19 19:32:40 +0900

    style : 상단바 UI

--- src/pages/ConcertInsidePage.tsx
+++ src/pages/ConcertInsidePage.tsx
@@ -19,22 +19,41 @@ function ConcertInsidePage() {
   }, []);
 
   return (
-    <div className="pb-64">
-      <ListHeader></ListHeader>
-      <ConcertInsideInfo concertId={Number(concertId)}></ConcertInsideInfo>
-      <ConcertCulture></ConcertCulture>
-      {status === ConcertStatus.UPCOMING ? (
-        <>
-          <ExpectationSetList
-            concertId={Number(concertId)}
-          ></ExpectationSetList>
-          <PastSetList concertId={Number(concertId)}></PastSetList>
-        </>
-      ) : (
-        <OngoingSetList concertId={Number(concertId)}></OngoingSetList>
-      )}
+    <div className="pb-90">
+      <ListHeader title={"공연 상세정보"} />
     </div>
   );
 }
 
 export default ConcertInsidePage;
+
+// function ConcertInsidePage() {
+//   const { concertId } = useParams<{ concertId: string }>();
+//   const location = useLocation();
+//   const status = location.state?.status;
+
+//   // 페이지 진입 시 스크롤 맨 위로 이동
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, []);
+
+//   return (
+//     <div className="pb-64">
+//       <ListHeader></ListHeader>
+//       <ConcertInsideInfo concertId={Number(concertId)}></ConcertInsideInfo>
+//       <ConcertCulture></ConcertCulture>
+//       {status === ConcertStatus.UPCOMING ? (
+//         <>
+//           <ExpectationSetList
+//             concertId={Number(concertId)}
+//           ></ExpectationSetList>
+//           <PastSetList concertId={Number(concertId)}></PastSetList>
+//         </>
+//       ) : (
+//         <OngoingSetList concertId={Number(concertId)}></OngoingSetList>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default ConcertInsidePage;
