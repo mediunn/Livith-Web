b56e10d5172640d17e5013d72fb2cdd04fbcebe9
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-07-20 20:36:21 +0900

    style : 콘서트 필수 정보 캐러셀

--- src/entities/concert/ui/ConcertTabPanel.tsx
+++ src/entities/concert/ui/ConcertTabPanel.tsx
@@ -1,7 +1,24 @@
 // import ConcertSchedulePanel from "../../../features/concert/ui/ConcertSchedulePanel";
 
+import ConcertInfoCarousel from "../../../widgets/ConcertInfoCarousel";
+
 function ConcertTabPanel() {
-  return <>{/* <ConcertSchedulePanel /> */}</>;
+  return (
+    <>
+      {/* <ConcertSchedulePanel /> */}
+
+      <div className="mx-16">
+        <div className="pt-30 pb-20">
+          <p className="text-grayScaleWhite text-body-lg font-semibold font-NotoSansKR">
+            콘서트 필수 정보를
+            <br />
+            확인해 보세요
+          </p>
+        </div>
+        <ConcertInfoCarousel />
+      </div>
+    </>
+  );
 }
 
 export default ConcertTabPanel;
