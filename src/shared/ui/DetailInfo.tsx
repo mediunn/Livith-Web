84dd64ebc9631f66141bb99b1f06220fb1a26338
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-07-30 01:28:43 +0900

    feat : 특정 콘서트 상세 정보 조회 api

--- src/shared/ui/DetailInfo.tsx
+++ src/shared/ui/DetailInfo.tsx
@@ -1,21 +1,24 @@
-import { ConcertFilter } from "../../entities/concert/types";
 import WebSiteEarthIcon from "../../shared/assets/WebSiteEarthIcon.svg";
 import WebSiteArrowIcon from "../../shared/assets/WebSiteArrowIcon.svg";
 
 interface DetailInfoProps {
   imageUrl: string;
-  status: ConcertFilter | string;
+  artist: string;
   title: string;
   date: string;
-  artist: string;
+  venue: string;
+  ticketSite: string;
+  ticketUrl: string;
 }
 
 function DetailInfo({
   imageUrl,
-  status,
+  artist,
   title,
   date,
-  artist,
+  venue,
+  ticketSite,
+  ticketUrl,
 }: DetailInfoProps) {
   return (
     <div className="w-full h-337 relative ">
@@ -35,39 +38,26 @@ function DetailInfo({
           {date}
         </p>
         <p className="pt-4 text-grayScaleBlack30 text-body-lgs font-regular font-NotoSansKR">
-          올림픽공원 올림픽홀
+          {venue}
         </p>
-        <button className="w-327 h-37 mt-16 flex items-center justify-center gap-4 text-grayScaleBlack100 text-body-sm font-semibold font-NotoSansKR bg-mainYellow30 rounded-6 border-none cursor-pointer">
+        <a
+          href={ticketUrl}
+          target="_blank"
+          className="w-327 h-37 mt-16 flex items-center justify-center gap-4 text-grayScaleBlack100 text-body-sm font-semibold font-NotoSansKR bg-mainYellow30 rounded-6 border-none cursor-pointer"
+        >
           <img
             src={WebSiteEarthIcon}
             alt="web site earth"
             className="w-18 h-18"
           />
-          Gen Hoshino presents MAD HOPE Asia Tour ...
+          {ticketSite}
           <img
             src={WebSiteArrowIcon}
             alt="web site arrow"
             className="w-8 h-8"
           />
-        </button>
+        </a>
       </div>
-
-      {/* <div className="absolute top-180 left-16 mr-16 ">
-        <div className="inline-flex items-center justify-center h-32 bg-grayScaleBlack90 rounded-24 px-13">
-          <p className="text-grayScaleBlack30 text-caption-lg font-semibold font-NotoSansKR m-0">
-            {status}
-          </p>
-        </div>
-        <p className=" my-6 text-grayScaleWhite text-head-lg font-semibold font-NotoSansKR">
-          {title}
-        </p>
-        <p className="mb-2 text-grayScaleWhite text-body-sm font-ragular font-NotoSansKR">
-          {date}
-        </p>
-        <p className="m-0 text-grayScaleWhite text-body-sm font-ragular font-NotoSansKR">
-          {artist}
-        </p>
-      </div> */}
     </div>
   );
 }
