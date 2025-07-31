36abb6b074857051778c7a0ace553a7d011c3b6e
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-08-01 03:15:31 +0900

    style : 콘서트 일정정보 탭 ui 수정

--- src/features/concert/ui/ConcertSetting.tsx
+++ src/features/concert/ui/ConcertSetting.tsx
@@ -146,11 +146,6 @@ function ConcertSetting({ schedules }: ConcertSettingProps) {
               padding: "0",
             }}
           >
-            <div className="pt-24 pl-16 pr-16 text-grayScaleWhite text-body-lg font-semibold font-NotoSansKR">
-              다가온 콘서트 일정을
-              <br />
-              확인해 보세요!
-            </div>
             <ScheduleInfo schedules={schedules} />
             {/* <EmptyConcertSchedulePanel /> */}
           </TabPanel>
