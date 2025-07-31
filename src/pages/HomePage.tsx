6d5d8821954c66edcf5ba3982c0282eb24087756
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-07-31 10:39:07 +0900

    fix : ScheduleInfo prop 에러 임시 해결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import SearchBar from "../shared/ui/SearchBar";
 import ConcertSettingEmpty from "../features/concert/ui/ConcertSettingEmpty";
 import ConcertSetting from "../features/concert/ui/ConcertSetting";
 import TabBar from "../shared/ui/TabBar";
+import { getSchedule, Schedule } from "../entities/concert/api/getSchedule";
 
 function HomePage() {
+  const { concertId } = useParams<{ concertId: string }>();
+  const [schedules, setSchedule] = useState<Schedule[]>([]);
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      try {
+        const data = await getSchedule(Number(concertId));
+        setSchedule(data);
+      } catch (error) {
+        console.error("특정 콘서트 일정 목록 조회 API 호출 실패:", error);
+        setSchedule([]);
+      }
+    };
+
+    fetchSchedule();
+  }, [concertId]);
+
   return (
     <div className="pb-90">
       <SearchBar></SearchBar>
       {/* <ConcertSettingEmpty></ConcertSettingEmpty> */}
-      <ConcertSetting></ConcertSetting>
+      <ConcertSetting schedules={schedules}></ConcertSetting>
       <TabBar></TabBar>
     </div>
   );
