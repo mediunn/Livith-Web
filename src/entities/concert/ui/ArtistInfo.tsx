b5a5e2fd472a933be7c0110884a32d5865c0f0f3
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-08-08 19:25:55 +0900

    style : 가수 정보 렌더링 조건 추가

--- src/entities/concert/ui/ArtistInfo.tsx
+++ src/entities/concert/ui/ArtistInfo.tsx
@@ -51,11 +51,13 @@ function ArtistInfo({
 
             <div className="px-16 py-16 ">
               <div className="relative">
-                <div className="inline-flex items-center justify-center bg-grayScaleBlack100 rounded-24">
-                  <p className="px-13 py-4 text-grayScaleBlack50 text-Caption1-Bold font-bold font-NotoSansKR">
-                    {category}
-                  </p>
-                </div>
+                {category && (
+                  <div className="inline-flex items-center justify-center bg-grayScaleBlack100 rounded-24">
+                    <p className="px-13 py-4 text-grayScaleBlack50 text-Caption1-Bold font-bold font-NotoSansKR">
+                      {category}
+                    </p>
+                  </div>
+                )}
                 <p className="pt-8 text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR">
                   {artist}
                 </p>
@@ -80,30 +82,36 @@ function ArtistInfo({
                 <p className="pt-12 text-grayScaleBlack30 text-Body4-md font-medium font-NotoSansKR">
                   {detail}
                 </p>
-                <div className="flex pt-20">
-                  <p className="w-37 text-grayScaleBlack30 text-Body4-md font-medium font-NotoSansKR">
-                    데뷔
-                  </p>
-                  <p className="text-grayScaleBlack50 text-Body4-md font-medium font-NotoSansKR">
-                    {formatBirthDate(birthDate)}, {birthPlace}
-                  </p>
-                </div>
+                {(birthDate || birthPlace) && (
+                  <div className="flex pt-20">
+                    <p className="w-37 text-grayScaleBlack30 text-Body4-md font-medium font-NotoSansKR">
+                      데뷔
+                    </p>
+                    <p className="text-grayScaleBlack50 text-Body4-md font-medium font-NotoSansKR">
+                      {birthDate ? formatBirthDate(birthDate) : ""}
+                      {birthDate && birthPlace ? ", " : ""} {birthPlace}
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </div>
 
         <div className="mt-13">
-          {keywords.map((keyword, index) => (
-            <div
-              key={index}
-              className="mr-4 mb-6 inline-flex items-center justify-center bg-grayScaleBlack80 rounded-24"
-            >
-              <p className="px-13 py-8 text-grayScaleBlack30 text-Caption1-Bold font-bold font-NotoSansKR">
-                {keyword}
-              </p>
-            </div>
-          ))}
+          {keywords.map(
+            (keyword, index) =>
+              keyword && (
+                <div
+                  key={index}
+                  className="mr-4 mb-6 inline-flex items-center justify-center bg-grayScaleBlack80 rounded-24"
+                >
+                  <p className="px-13 py-8 text-grayScaleBlack30 text-Caption1-Bold font-bold font-NotoSansKR">
+                    {keyword}
+                  </p>
+                </div>
+              )
+          )}
         </div>
       </div>
     </>
