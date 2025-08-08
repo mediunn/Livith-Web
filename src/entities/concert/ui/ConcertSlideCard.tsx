c8eda415881c5c3fb4883ad303f5ea57653db314
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-08-08 19:00:29 +0900

    style : 콘서트 상태 설정 통일

--- src/entities/concert/ui/ConcertSlideCard.tsx
+++ src/entities/concert/ui/ConcertSlideCard.tsx
@@ -1,11 +1,12 @@
-import { ConcertFilter } from "../types";
+import { ConcertFilter, ConcertStatus } from "../types";
 import EmptyConcertCardIcon from "../../../shared/assets/EmptyConcertCardIcon.svg";
+import { setConcertStatus } from "../../../features/search/utils/setConcertStatus";
 
 type ConcertSlideCardProps = {
   imageUrl?: string;
   title: string;
   date: string;
-  filter: ConcertFilter;
+  status: ConcertStatus;
   daysLeft: number;
   onClick?: () => void;
 };
@@ -14,24 +15,10 @@ function ConcertSlideCard({
   imageUrl,
   title,
   date,
-  filter,
+  status,
   daysLeft,
   onClick,
 }: ConcertSlideCardProps) {
-  let filterText = "";
-
-  if (filter === ConcertFilter.UPCOMING) {
-    filterText = `D-${daysLeft}`;
-  } else {
-    if (daysLeft > 0) {
-      filterText = `D-${daysLeft}`;
-    } else if (daysLeft === 0) {
-      filterText = "진행중";
-    } else {
-      filterText = "종료";
-    }
-  }
-
   return (
     <div className="w-139 h-280 cursor-pointer" onClick={onClick}>
       <div className="w-139 h-196 relative">
@@ -51,7 +38,7 @@ function ConcertSlideCard({
 
         <div className="absolute top-10 left-10 px-13 py-8 inline-flex items-center justify-center h-32 bg-grayScaleBlack90 rounded-24">
           <p className="text-grayScaleBlack30 text-Caption1-Bold font-bold font-NotoSansKR">
-            {filterText}
+            {setConcertStatus({ status, daysLeft })}
           </p>
         </div>
       </div>
