de736e1d2c4817ad70e2df13061489a564bb7919
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-08-30 05:29:31 +0900

    feat : Firebase 세팅 및 초기화

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-ueIi71ftAWxhwL_HH1_N07umX-Z_V9U",
+  authDomain: "livith-5327b.firebaseapp.com",
+  projectId: "livith-5327b",
+  storageBucket: "livith-5327b.firebasestorage.app",
+  messagingSenderId: "864062841722",
+  appId: "1:864062841722:web:0265798e165bc8ed692cc3",
+  measurementId: "G-7RTQE8ZLYW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
