de8438f4d64d7242ad7b62221bac3f79acd8c296
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-29 22:24:07 +0900

    feat: 콘서트 상세 페이지에서 관심있는 콘서트 변경 기능 추가

---
+++ src/widgets/ChangeConcertConfirmModal.tsx
@@ -0,0 +1,63 @@
+import { toast } from "react-toastify";
+import WarningIcon from "../shared/assets/WarningIcon.svg";
+import Lottie from "lottie-react";
+import InterestConcertToastIconMotion from "../shared/assets/InterestConcertToastIconMotion.json";
+interface ChangeConcertConfirmModalProps {
+  id: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+function ChangeConcertConfirmModal({
+  id,
+  isOpen,
+  onClose,
+}: ChangeConcertConfirmModalProps) {
+  const STORAGE_KEY = "InterestConcertId";
+  const handleChange = () => {
+    localStorage.setItem(STORAGE_KEY, id);
+    onClose();
+    toast(
+      <div className="flex items-center space-x-13 text-grayScaleWhite text-Body3 font-medium font-NotoSansKR">
+        <Lottie animationData={InterestConcertToastIconMotion} />
+        <span>관심 공연을 변경했어요</span>
+      </div>,
+      {
+        position: "top-center",
+        autoClose: 3000,
+      }
+    );
+  };
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div
+        onClick={onClose}
+        className="max-w-md m-auto fixed inset-0 bg-black bg-opacity-50 z-[70] cursor-pointer"
+      />
+      <div className="fixed z-[71]">
+        <div className="w-[327px] max-w-[87%] h-fit fixed flex flex-col bg-grayScaleWhite rounded-11 pb-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <img src={WarningIcon} alt="Warning Icon" className="mx-auto mt-20" />
+          <p className="text-grayScaleBlack100 text-Body2-md font-medium font-NotoSansKR text-center mt-17">
+            관심 콘서트를 변경하시나요?
+          </p>
+          <div className="flex flex-row justify-center space-x-14 mt-20 ">
+            <button
+              className="bg-grayScaleBlack5 text-caution100 text-Body4-re font-regular font-NotoSansKR rounded-8 py-18 px-42"
+              onClick={handleChange}
+            >
+              변경할래요
+            </button>
+            <button
+              className="bg-grayScaleBlack80 text-grayScaleWhite text-Body4-re font-regular font-NotoSansKR rounded-8 py-18 px-28"
+              onClick={onClose}
+            >
+              변경하지 않아요
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ChangeConcertConfirmModal;
