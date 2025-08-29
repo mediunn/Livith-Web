2dfff9edbb986bc180c8ad97422de43d5bce4b5b
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-08-29 22:40:56 +0900

    feat : 배너 조회 API

--- src/features/concert/api/getBanner.ts
+++ src/features/concert/api/getBanner.ts
@@ -11,7 +11,7 @@ type Banner = {
 
 export async function getBanner(): Promise<Banner[]> {
   const response = await axiosInstance.get<ApiResponse<Banner[]>>(
-    "/api/v3/category/banners"
+    "/api/v3/search/banners"
   );
   return response.data.data;
 }
