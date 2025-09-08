d652b16bba81e9ff4b66725827afe4f0f785db44
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-09-09 00:21:18 +0900

    fix: 관심 콘서트 삭제 팝업 여러 번 막기

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,9 @@ import { useTabDirection } from "../shared/hooks/useTabDirection";
 import { getConcertInsideInfo } from "../entities/concert/api/getConcertInsideInfo";
 import { Concert } from "../entities/concert/types";
 import { getSchedule, Schedule } from "../entities/concert/api/getSchedule";
+import { toast } from "react-toastify";
+import Lottie from "lottie-react";
+import DeleteConcertToastIconMotion from "../shared/assets/DeleteConcertToastIconMotion.json";
 
 function HomePage() {
   const { direction, currentTab, updateDirection } = useTabDirection();
@@ -49,6 +52,33 @@ function HomePage() {
 
     fetchSchedule();
   }, [interestConcertId]);
+
+  // 관심 콘서트 삭제 토스트
+  useEffect(() => {
+    if (localStorage.getItem("deleteConcertToast") === "true") {
+      toast(
+        <div className="flex items-center space-x-13 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
+          <Lottie
+            animationData={DeleteConcertToastIconMotion}
+            loop={false}
+            renderer="svg"
+            rendererSettings={{
+              preserveAspectRatio: "xMidYMid meet",
+            }}
+          />
+          <span>관심 콘서트가 삭제되었어요</span>
+        </div>,
+        {
+          position: "top-center",
+          autoClose: 3000,
+          pauseOnFocusLoss: false, // 창이 다른 곳에 있어도 시간 그대로 감
+          onClose: () => {
+            localStorage.removeItem("deleteConcertToast");
+          },
+        }
+      );
+    }
+  }, []);
 
   return (
     <div className="pb-90">
