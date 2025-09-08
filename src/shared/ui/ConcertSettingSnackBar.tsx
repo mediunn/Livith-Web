01eb96c265d3bb275cbe10e39daa1aa355704100
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-09-08 17:54:40 +0900

    feat : 스낵바 모션 추가

--- src/shared/ui/ConcertSettingSnackBar.tsx
+++ src/shared/ui/ConcertSettingSnackBar.tsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { toast } from "react-toastify";
 import Lottie from "lottie-react";
 import InterestConcertToastIconMotion from "../../shared/assets/InterestConcertToastIconMotion.json";
 import { logEvent } from "firebase/analytics";
 import { analytics } from "../../app/firebase";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface ConcertSettingSnackBarProps {
   id: string | number;
@@ -34,30 +36,50 @@ function ConcertSettingSnackBar({
         autoClose: 3000,
       }
     );
-
-    // 버튼 클릭 후 4초 뒤에 SnackBar 닫기
-    setTimeout(() => {
-      onClose();
-    }, 4000);
   };
 
+  useEffect(() => {
+    // 스낵바 5초 동안 유지
+    const timer = setTimeout(() => {
+      onClose();
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
   return (
-    <div className="flex justify-between items-start px-20 py-12 w-full bg-grayScaleBlack80 rounded-8">
-      <div>
-        <p className="text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
-          웹사이트를 보셨나요?
-        </p>
-        <p className="text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
-          관심 콘서트 설정하고 공연 알림을 받으세요
-        </p>
-      </div>
-      <button
-        onClick={handleChange}
-        className="pt-4 text-mainYellow30 text-Caption1-sm font-semibold font-NotoSansKR"
+    <AnimatePresence>
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{
+          y: 100,
+          opacity: 0,
+          transition: { duration: 0.5, ease: "easeOut" }, // 0.5초 동안 easeOut
+        }}
+        transition={{
+          // 0.8초 후 0.5초 동안 easeOut
+          delay: 0.8,
+          duration: 0.5,
+          ease: "easeOut",
+        }}
+        className="flex justify-between items-start px-20 py-12 w-full bg-grayScaleBlack80 rounded-8"
       >
-        콘서트 설정
-      </button>
-    </div>
+        <div>
+          <p className="text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
+            웹사이트를 보셨나요?
+          </p>
+          <p className="text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
+            관심 콘서트 설정하고 공연 알림을 받으세요
+          </p>
+        </div>
+        <button
+          onClick={handleChange}
+          className="pt-4 text-mainYellow30 text-Caption1-sm font-semibold font-NotoSansKR"
+        >
+          콘서트 설정
+        </button>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
