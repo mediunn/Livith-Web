3f71dcbde03c42522d8c31e49ffe5b22bb7d2a4e
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-10-09 11:33:53 +0900

    style : 댓글 작성 필드 고정 및 그라데이션 추가

--- src/entities/concert/ui/CommentInputBar.tsx
+++ src/entities/concert/ui/CommentInputBar.tsx
@@ -23,30 +23,34 @@ function CommentInputBar() {
   const isActive = value.length > 0 && value.length <= 400;
 
   return (
-    <div className="absolute bottom-0 px-16 py-10 flex justify-between items-end w-full">
-      <div className="flex items-center flex-1 px-16 py-14 bg-grayScaleBlack90 rounded-10">
-        <textarea
-          ref={textareaRef}
-          value={value}
-          placeholder="로그인 후 작성 가능해요"
-          onChange={(e) => setValue(e.target.value)}
-          className="bg-transparent outline-none text-grayScaleWhite text-Body3-md font-medium font-NotoSansKR placeholder-grayScaleBlack50 w-full resize-none overflow-y-auto"
-          rows={1}
-          style={{
-            lineHeight: "21px",
-            maxHeight: "84px",
-          }}
-        />
-      </div>
-      <button
-        disabled={!isActive}
-        className={`px-16 py-14 ml-12 max-h-49 rounded-10 font-medium text-Body3-md font-NotoSansKR 
+    <>
+      <div className="fixed bottom-69 h-52 w-full max-w-md bg-gradient-to-t from-grayScaleBlack100 to-transparent" />
+
+      <div className="fixed bottom-0 px-16 py-10 w-full max-w-md bg-grayScaleBlack100 flex justify-between items-end">
+        <div className="flex items-center flex-1 px-16 py-14 bg-grayScaleBlack90 rounded-10">
+          <textarea
+            ref={textareaRef}
+            value={value}
+            placeholder="로그인 후 작성 가능해요"
+            onChange={(e) => setValue(e.target.value)}
+            className="bg-transparent outline-none text-grayScaleWhite text-Body3-md font-medium font-NotoSansKR placeholder-grayScaleBlack50 w-full resize-none overflow-y-auto"
+            rows={1}
+            style={{
+              lineHeight: "21px",
+              maxHeight: "84px",
+            }}
+          />
+        </div>
+        <button
+          disabled={!isActive}
+          className={`px-16 py-14 ml-12 max-h-49 rounded-10 font-medium text-Body3-md font-NotoSansKR 
           ${isActive ? "bg-mainYellow30 text-grayScaleBlack100" : "bg-grayScaleBlack80 text-grayScaleBlack50"}
         `}
-      >
-        등록
-      </button>
-    </div>
+        >
+          등록
+        </button>
+      </div>
+    </>
   );
 }
 
