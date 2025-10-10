c09646f981a96b9ed4eda5078ad09a1834d66498
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-10-10 23:59:31 +0900

    style : 댓글 삭제 팝업

--- src/entities/concert/ui/Comment.tsx
+++ src/entities/concert/ui/Comment.tsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
 import ProfileIcon from "../../../shared/assets/ProfileIcon.svg";
+import DeleteCommentModal from "../../../widgets/DeleteCommentModal";
 
 function Comment() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="px-16 py-21 bg-grayScaleBlack90 rounded-6">
-      <div className="flex justify-between items-end items-center">
-        <div className="flex items-center">
-          <img src={ProfileIcon} className="w-32 h-32" />
-          <p className="pl-8 text-grayScaleBlack30 text-Body3-sm font-semibold font-NotoSansKR">
-            닉네임
-          </p>
+    <>
+      <div className="px-16 py-21 bg-grayScaleBlack90 rounded-6">
+        <div className="flex justify-between items-end items-center">
+          <div className="flex items-center">
+            <img src={ProfileIcon} className="w-32 h-32" />
+            <p className="pl-8 text-grayScaleBlack30 text-Body3-sm font-semibold font-NotoSansKR">
+              닉네임
+            </p>
+          </div>
+          <button
+            onClick={() => {
+              setIsModalOpen(true);
+            }}
+            className="bg-grayScaleBlack100 rounded-24 px-12 py-4 text-grayScaleBlack80 text-Caption1-Bold font-bold font-NotoSansKR"
+          >
+            삭제
+          </button>
         </div>
-        <button className="bg-grayScaleBlack100 rounded-24 px-12 py-4 text-grayScaleBlack80 text-Caption1-Bold font-bold font-NotoSansKR">
-          신고
-        </button>
+        <p className="pt-12 text-grayScaleWhite text-Body2-re font-regular font-NotoSansKR">
+          댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ
+        </p>
       </div>
-      <p className="pt-12 text-grayScaleWhite text-Body2-re font-regular font-NotoSansKR">
-        댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ댓글내용임ㅇㅇ
-      </p>
-    </div>
+      <DeleteCommentModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 }
 
---
+++ src/widgets/DeleteCommentModal.tsx
@@ -0,0 +1,75 @@
+import WarningIcon from "../shared/assets/WarningIcon.svg";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface DeleteCommentModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function DeleteCommentModal({ isOpen, onClose }: DeleteCommentModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <>
+          {/* 배경 어둡게 Fade out 0.1 */}
+          <motion.div
+            onClick={onClose}
+            className="max-w-md m-auto fixed inset-0 bg-black z-[70] cursor-pointer"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.5 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1, ease: "easeOut" }}
+          />
+
+          <motion.div
+            className="fixed z-[71]"
+            initial={{ opacity: 0 }}
+            // 팝업 열릴 때
+            animate={{
+              opacity: 1,
+              transition: {
+                duration: 0.15,
+                type: "spring",
+                stiffness: 756,
+                damping: 48,
+                mass: 1,
+              },
+            }}
+            // 팝업 닫힐 때
+            exit={{
+              opacity: 0,
+              transition: {
+                duration: 0.15,
+                ease: "easeOut",
+              },
+            }}
+          >
+            <div className="w-[327px] max-w-[87%] h-fit fixed flex flex-col bg-grayScaleWhite rounded-11 pb-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <img src={WarningIcon} className="mx-auto mt-16" />
+              <p className="text-grayScaleBlack100 text-Body2-md font-medium font-NotoSansKR text-center mt-10">
+                댓글을 삭제하시겠어요?
+              </p>
+              <div className="flex flex-row justify-center space-x-14 mt-20 ">
+                <button className="bg-grayScaleBlack5 text-caution100 text-Body4-re font-regular font-NotoSansKR rounded-8 py-18 px-18">
+                  지금은 삭제할래요
+                </button>
+                <button
+                  className="bg-grayScaleBlack80 text-grayScaleWhite text-Body4-re font-regular font-NotoSansKR rounded-8 py-18 px-28"
+                  onClick={() => {
+                    window.amplitude.track("cancel_change_interest");
+                    onClose();
+                  }}
+                >
+                  잘못 눌렀어요
+                </button>
+              </div>
+            </div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+}
+export default DeleteCommentModal;
