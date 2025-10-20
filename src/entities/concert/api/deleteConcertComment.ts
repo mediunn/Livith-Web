87bfaa71aa621e5e87b78b6947d01f0c616260d3
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-10-21 03:02:28 +0900

    fix : 특정 댓글 삭제 API 수정

--- src/entities/concert/api/deleteConcertComment.ts
+++ src/entities/concert/api/deleteConcertComment.ts
@@ -4,7 +4,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 
 export async function deleteConcertComment(id: number) {
-  const response = await axios.delete(`${SERVER_URL}/api/v4/comments/${id}`, {
+  const response = await axios.delete(`${SERVER_URL}api/v4/comments/${id}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
