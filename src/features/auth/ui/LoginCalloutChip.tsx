3d4f852e6160bee2037022dc5f0dddaa92590bec
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-21 22:21:57 +0900

    feat: 로그인 전 마이페이지 UI 구현 및 카카오 로그인 API 연결

---
+++ src/features/auth/ui/LoginCalloutChip.tsx
@@ -0,0 +1,24 @@
+import TabBar from "../../../shared/ui/TabBar";
+import CalloutChip from "../../../shared/assets/CalloutChip.svg";
+function LoginCalloutChip() {
+  const recentLogin = localStorage.getItem("recentLogin");
+  return (
+    <div className="relative flex justify-center">
+      <img src={CalloutChip} className="mx-[20%] mb-20" />
+      {recentLogin ? (
+        <p className="absolute top-7  text-grayScaleBlack50 text-Caption1-Bold font-bold">
+          <span className="text-grayScaleWhite">{recentLogin}</span>로 최근에
+          로그인 했어요
+        </p>
+      ) : (
+        <p className="absolute top-7  text-grayScaleBlack50 text-Caption1-Bold font-bold">
+          회원가입하고{" "}
+          <span className="text-grayScaleWhite">모든 서비스 이용</span>해보세요!
+        </p>
+      )}
+      <TabBar />
+    </div>
+  );
+}
+
+export default LoginCalloutChip;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,14 +1,29 @@
 import Nickname from "../features/my/ui/Nickname";
 import TabBar from "../shared/ui/TabBar";
 import Info from "../features/my/ui/Info";
+import { useEffect, useState } from "react";
+import LoggedOutMyPage from "./LoggedOutMyPage";
 
 function MyPage() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    setIsLoggedIn(!!token);
+  }, []);
+
   return (
-    <div className="pb-90">
-      <Nickname></Nickname>
-      <Info></Info>
-      <TabBar />
-    </div>
+    <>
+      {isLoggedIn ? (
+        <div className="pb-90">
+          <Nickname></Nickname>
+          <Info></Info>
+          <TabBar />
+        </div>
+      ) : (
+        <LoggedOutMyPage />
+      )}
+    </>
   );
 }
 
