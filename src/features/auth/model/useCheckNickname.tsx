dffbe6e938359c4ab9d2296b81b1bb66f675baa6
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-26 12:53:49 +0900

    feat: 닉네임 수정 페이지 UI 구현 및 API 연결

--- src/features/auth/model/useCheckNickname.tsx
+++ src/features/auth/model/useCheckNickname.tsx
@@ -3,9 +3,10 @@ import { checkNickname } from "../api/checkNickname";
 
 export function useCheckNickname(nickname: string) {
   return useQuery({
-    queryKey: ["checkNickname", nickname],
+    queryKey: ["checkNickname", nickname], // 닉네임 포함
     queryFn: () => checkNickname(nickname),
-    enabled: false, // 처음엔 실행 안 함
+    enabled: false, // 수동 실행
     select: (data) => data.data,
+    gcTime: 0, // 캐시 저장 안 함
   });
 }
---
+++ src/shared/ui/ErrorToast.tsx
@@ -0,0 +1,10 @@
+import ErrorToastIcon from "../assets/ErrorToastIcon.svg";
+
+export default function CompleteToast({ message }: { message: string }) {
+  return (
+    <div className="flex items-center space-x-13 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
+      <img src={ErrorToastIcon} className="w-24 h-24" />
+      <span>{message}</span>
+    </div>
+  );
+}
