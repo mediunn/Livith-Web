c13fd8928d31391d4d4c7fbcdd240b20b437bb5c
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-26 13:00:10 +0900

    refactor: 에러 토스트 함수명 수정

--- src/shared/ui/ErrorToast.tsx
+++ src/shared/ui/ErrorToast.tsx
@@ -1,6 +1,6 @@
 import ErrorToastIcon from "../assets/ErrorToastIcon.svg";
 
-export default function CompleteToast({ message }: { message: string }) {
+export default function ErrorToast({ message }: { message: string }) {
   return (
     <div className="flex items-center space-x-13 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
       <img src={ErrorToastIcon} className="w-24 h-24" />
