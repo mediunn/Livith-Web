fd245157ec5a92df0b2f8e2ace8f4d80567ffe84
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-27 23:00:56 +0900

    feat: 회원탈퇴 API 연결

--- src/shared/ui/CompleteToast.tsx
+++ src/shared/ui/CompleteToast.tsx
@@ -15,7 +15,7 @@ export default function CompleteToast({ message }: { message: string }) {
           }}
         />
       </div>
-      <span>{message}</span>
+      <div style={{ whiteSpace: "pre-line" }}>{message}</div>
     </div>
   );
 }
