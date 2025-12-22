4a278f02842381c40c7ebdca1886514506b19e75
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-12-22 20:16:38 +0900

    feat : 도자캣 이벤트 팝업 제거

--- src/pages/ConcertInsidePage.tsx
+++ src/pages/ConcertInsidePage.tsx
@@ -9,7 +9,6 @@ import LoginPromptToast from "../shared/ui/LoginPromptToast";
 import LoginModal from "../features/auth/ui/LoginModal";
 import { useRecoilState } from "recoil";
 import { userState } from "../entities/recoil/atoms/userState";
-import EventModal from "../shared/ui/EventModal";
 
 function ConcertInsidePage() {
   const { concertId } = useParams<{ concertId: string }>();
@@ -22,17 +21,6 @@ function ConcertInsidePage() {
     const stored = sessionStorage.getItem("viewedConcerts");
     return stored ? JSON.parse(stored) : [];
   });
-
-  // 이벤트 팝업
-  const [isEventModalOpen, setIsEventModalOpen] = useState(false);
-
-  useEffect(() => {
-    if (concertId === "1549") {
-      setIsEventModalOpen(true);
-    } else {
-      setIsEventModalOpen(false);
-    }
-  }, [concertId]);
 
   // 페이지 진입 시 스크롤 맨 위로 이동
   useEffect(() => {
@@ -93,12 +81,6 @@ function ConcertInsidePage() {
         onClose={() => setIsLoginModalOpen(false)}
         type="concertInfo"
       />
-      {concertId === "1549" && (
-        <EventModal
-          isOpen={isEventModalOpen}
-          onClose={() => setIsEventModalOpen(false)}
-        />
-      )}
     </div>
   );
 }
