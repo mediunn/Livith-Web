716ae0143a7f564a0e41c4e39cb2ccd984b633dd
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-01-09 23:48:37 +0900

    refactor: 스낵바 컴포넌트화

--- src/shared/ui/Snackbar/Snackbar.tsx
+++ src/shared/ui/Snackbar/Snackbar.tsx
@@ -1,22 +1,23 @@
-export default function LoginPromptToast({
-  message,
-  onLoginClick,
-}: {
+interface SnackbarProps {
   message: string;
-  onLoginClick: () => void;
-}) {
+  onButtonClick: () => void;
+  buttonMessage?: string;
+}
+
+export default function Snackbar({
+  message,
+  onButtonClick,
+  buttonMessage,
+}: SnackbarProps) {
   return (
     <>
       <div className="flex items-center space-x-13 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
-        <p>
-          {`${message}를 3개나 열람하셨네요!`} <br />
-          로그인 후 더 많은 기능을 이용해보세요
-        </p>
+        <p style={{ whiteSpace: "pre-line" }}>{message}</p>
         <button
-          onClick={onLoginClick}
+          onClick={onButtonClick}
           className="absolute top-16 right-16 cursor-pointer text-Caption1-sm font-semibold text-mainYellow30"
         >
-          로그인
+          {buttonMessage}
         </button>
       </div>
     </>
