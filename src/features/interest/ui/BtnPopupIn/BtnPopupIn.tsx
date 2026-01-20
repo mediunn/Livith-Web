b9def20ce84eeb3c34907f4f2ec1942fc1d68feb
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-01-20 20:16:45 +0900

    feat: btn_popup_in 스토리북 적용

--- src/features/interest/ui/BtnPopupIn/BtnPopupIn.tsx
+++ src/features/interest/ui/BtnPopupIn/BtnPopupIn.tsx
@@ -15,7 +15,7 @@ export default function BtnPopupIn({
   return (
     <div
       onClick={onClick}
-      className="hover:bg-grayScaleBlack100 flex flex-row py-15 space-x-16 px-17 cursor-pointer rounded-8"
+      className="hover:bg-grayScaleBlack100 bg-grayScaleBlack90 flex flex-row py-15 space-x-16 px-17 cursor-pointer rounded-8"
     >
       <img src={icon} />
       <div className={`${textColor} text-Body2-md font-medium font-NotoSansKR`}>
