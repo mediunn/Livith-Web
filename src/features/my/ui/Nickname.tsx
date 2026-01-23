feee580276db871cbbd128dcd74d1f4c582070ef
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-01-23 15:27:28 +0900

    style: btn_profile_change hover 적용

--- src/features/my/ui/Nickname.tsx
+++ src/features/my/ui/Nickname.tsx
@@ -24,7 +24,7 @@ function Nickname() {
           </div>
           <button
             onClick={() => navigate(`/nickname`)}
-            className="px-12 py-6 bg-grayScaleBlack80 rounded-17 text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR"
+            className="hover:bg-grayScaleBlack90 px-12 py-6 bg-grayScaleBlack80 rounded-17 text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR"
           >
             닉네임 수정
           </button>
