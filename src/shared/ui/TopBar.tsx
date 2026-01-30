50da72e8b4ece46e64d39d1261e778dce7188d23
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2026-01-30 19:32:15 +0900

    style : 새 알림 아이콘 추가

--- src/shared/ui/TopBar.tsx
+++ src/shared/ui/TopBar.tsx
@@ -1,5 +1,6 @@
 import LogoIcon from "../assets/LogoIcon.svg";
 import AlarmIcon from "../assets/AlarmIcon.svg";
+import NewAlarmIcon from "../assets/NewAlarmIcon.svg";
 
 interface TopBarProps {
   bgColor?: string;
@@ -11,7 +12,8 @@ function TopBar({ bgColor = "bg-transparent" }: TopBarProps) {
       className={`sticky top-0 z-50 px-16 flex items-center justify-between w-full h-60 ${bgColor}`}
     >
       <img src={LogoIcon} className={"w-101 h-25"} />
-      <img src={AlarmIcon} className={"w-38 h-38"} />
+      {/* <img src={AlarmIcon} className={"w-38 h-38"} /> */}
+      <img src={NewAlarmIcon} className={"w-38 h-38"} />
     </div>
   );
 }
