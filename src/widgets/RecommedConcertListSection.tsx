8a8c75ad785fd1cd6107e02a811cf0ed13c8e388
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2026-02-13 19:40:37 +0900

    fix : 홈 화면의 취향 반영 콘서트 목록 더보기 버튼 조건 추가

--- src/widgets/RecommedConcertListSection.tsx
+++ src/widgets/RecommedConcertListSection.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { useRecommendConcertListSection } from "../features/concert/model/useRecommendConcertListSection";
 import CardListSkeleton from "../shared/ui/CardSkeleton/CardListSkeleton";
 import RecommedConcertSlide from "../entities/concert/ui/RecommedConcertSlide";
+import ConcertListSectionArrowIcon from "../shared/assets/ConcertListSectionArrowIcon.svg";
 
 interface Props {
   nickname: string;
 }
 
 function RecommedConcertListSection({ nickname }: Props) {
+  const navigate = useNavigate();
+
   const { data: concerts = [], isLoading } =
     useRecommendConcertListSection(true);
 
@@ -14,11 +18,19 @@ function RecommedConcertListSection({ nickname }: Props) {
 
   return (
     <div className="mt-30">
-      <p className="mb-20 ml-16 text-grayScaleWhite text-Body1-sm font-semibold font-NotoSansKR">
-        {nickname}님의 <br />
-        취향이 담긴 콘서트
-      </p>
-
+      <div className="flex justify-between items-end pb-20 px-16">
+        <p className="text-grayScaleWhite text-Body1-sm font-semibold font-NotoSansKR">
+          {nickname}님의 <br />
+          취향이 담긴 콘서트
+        </p>
+        {concerts.length >= 10 && (
+          <img
+            src={ConcertListSectionArrowIcon}
+            className="w-24 h-24 cursor-pointer"
+            onClick={() => navigate("/concerts/recommed")}
+          />
+        )}
+      </div>
       {isLoading ? (
         <div className="mx-16">
           <CardListSkeleton num={3} />
