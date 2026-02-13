4252ac3866954b018f9c34f6ef8de8912675b699
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2026-02-13 20:17:57 +0900

    fix : 알림 읽음 즉각 반영

--- src/entities/notification/model/useUpdateRead.ts
+++ src/entities/notification/model/useUpdateRead.ts
@@ -1,8 +1,13 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateRead } from "../api/updateRead";
 
 export function useUpdateRead() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (id: number) => updateRead(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["alarms"] });
+    },
   });
 }
