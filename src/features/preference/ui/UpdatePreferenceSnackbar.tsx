d8b1d4b2b15c40b68951f19b174d6c4af5b1f4d7
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-02-14 16:17:07 +0900

    feat: 취향 설정 완료 스낵바 '설정'으로 네이밍 변경

--- src/features/preference/ui/UpdatePreferenceSnackbar.tsx
+++ src/features/preference/ui/UpdatePreferenceSnackbar.tsx
@@ -3,7 +3,7 @@ import Snackbar from "../../../shared/ui/Snackbar/Snackbar";
 function UpdatePreferenceSnackbar({ type }: { type: "장르" | "아티스트" }) {
   return (
     <Snackbar
-      message={`선호 ${type}를 변경했어요!\n홈에서 확인해 볼까요?`}
+      message={`선호 ${type}를 설정했어요!\n홈에서 확인해 볼까요?`}
       buttonMessage="홈으로 이동"
       onButtonClick={() => (window.location.href = "/")}
     />
