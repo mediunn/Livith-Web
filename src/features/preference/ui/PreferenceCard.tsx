be535c27ca7cf4c1426f8aab275445483df7d080
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-04-24 17:35:14 +0900

    feat: 취향 장르 목록 UI 수정

--- src/features/preference/ui/PreferenceCard.tsx
+++ src/features/preference/ui/PreferenceCard.tsx
@@ -51,7 +51,7 @@ function PreferenceCard({
       <img
         src={imgUrl}
         alt={label}
-        className="bg-grayScaleBlack100 opacity-70 "
+        className="bg-grayScaleBlack100 opacity-70 w-full h-full object-cover"
       />
       <span className="absolute left-0 right-0 top-1/2 -translate-y-1/2 w-full flex flex-col items-center justify-center text-center text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR select-none">
         {label}
