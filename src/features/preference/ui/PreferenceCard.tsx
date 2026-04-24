7ef102e7e36d07588abeec0fb39f1d72c3a519d4
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-04-24 17:43:41 +0900

    style: 장르 줄바꿈 수정

--- src/features/preference/ui/PreferenceCard.tsx
+++ src/features/preference/ui/PreferenceCard.tsx
@@ -53,7 +53,7 @@ function PreferenceCard({
         alt={label}
         className="bg-grayScaleBlack100 opacity-70 w-full h-full object-cover"
       />
-      <span className="absolute left-0 right-0 top-1/2 -translate-y-1/2 w-full flex flex-col items-center justify-center text-center text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR select-none">
+      <span className="absolute left-0 right-0 top-1/2 -translate-y-1/2 mx-16 flex flex-col items-center justify-center text-center text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR select-none">
         {label}
       </span>
     </div>
