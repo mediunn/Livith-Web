e70402cf972e93e1ab65bc54d904dc30b58341b6
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2026-04-24 21:39:19 +0900

    feat : 관심 콘서트 설정 버튼 라이팅 수정

--- src/features/concert/ui/ConcertSettingEmpty.tsx
+++ src/features/concert/ui/ConcertSettingEmpty.tsx
@@ -70,16 +70,12 @@ function ConcertSettingEmpty({ group, hasPrefer }: ConcertSettingEmptyProps) {
                 loop={true}
                 className="w-40 h-40"
               />
-              {group === "A" && !user ? (
-                <p className="mt-7 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
-                  30초 로그인 후 <br />
-                  관심 콘서트 설정
-                </p>
-              ) : (
-                <p className="mt-7 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
-                  관심 콘서트 설정
-                </p>
-              )}
+
+              <p className="mt-7 text-grayScaleWhite text-Body4-sm font-semibold font-NotoSansKR">
+                관심 콘서트
+                <br />
+                알림 · 소식 설정
+              </p>
             </div>
           </button>
         </div>
