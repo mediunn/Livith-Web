43366fc29418411029ad93bf02aa058f9168bd91
Lee Ahyoung <ahyoung12@gachon.ac.kr>
2026-05-10 19:59:36 +0900

    fix : 관심 콘서트 취소 토스트

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -98,10 +98,28 @@ function HomePage() {
 
     hasShownAutoCleanToastRef.current = true;
 
-    toast(<CompleteToast message="종료된 공연이 자동 정리됐어요" />, {
-      position: "top-center",
-      autoClose: 3000,
-    });
+    const type = concertToastData.data.type;
+
+    if (type === "BOTH") {
+      toast(<CompleteToast message="종료된 공연이 자동 정리됐어요" />, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+      toast(<CompleteToast message="취소된 공연이 자동 정리됐어요" />, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+    } else {
+      const message =
+        type === "CANCELED"
+          ? "취소된 공연이 자동 정리됐어요"
+          : "종료된 공연이 자동 정리됐어요";
+
+      toast(<CompleteToast message={message} />, {
+        position: "top-center",
+        autoClose: 3000,
+      });
+    }
 
     patchConcertToast();
   }, [concertToastData, patchConcertToast]);
