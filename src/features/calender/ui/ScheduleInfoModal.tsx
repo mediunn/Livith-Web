6686463c16813b429f65964f95f96714ab4c80bd
Lee Ahyoung <ahyoung12@gachon.ac.kr>
2026-07-19 20:08:42 +0900

    style : ScheduleInfoItem 컴포넌트화 및 호버 시 색상 변경

--- src/features/calender/ui/ScheduleInfoModal.tsx
+++ src/features/calender/ui/ScheduleInfoModal.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import GuidedBannerCloseIcon from "../../../shared/assets/GuidedBannerCloseIcon.svg";
 import CalenderInfoArrowIcon from "../../../shared/assets/CalenderInfoArrowIcon.svg";
+import ScheduleInfoItem from "../../../shared/ui/ScheduleInfoItem";
 
 interface CommonModalProps {
   isOpen: boolean;
@@ -54,135 +55,37 @@ function ScheduleInfoModal({ isOpen, onClose }: CommonModalProps) {
                   <img src={GuidedBannerCloseIcon} className="w-full h-full" />
                 </button>
               </div>
-              <div className="flex flex-col pt-16">
-                <div className="flex items-center">
-                  <div className="w-4 h-12 bg-lyricsTranslation rounded-1 mr-6" />
-                  <p className="text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR">
-                    18:00
-                  </p>
-                </div>
+              <ScheduleInfoItem
+                indicatorColor="bg-lyricsTranslation"
+                time="18:00"
+                badgeText="예매일"
+                title="위켄드 내한공연 2026"
+                description="NOL 티켓"
+              />
 
-                <div className="flex items-center justify-between bg-grayScaleBlack80 rounded-8 mt-10 px-12 py-12 cursor-pointer">
-                  <div>
-                    <div
-                      className={`inline-flex items-center justify-center rounded-24 bg-grayScaleBlack90 `}
-                    >
-                      <p
-                        className={`px-10 py-4 text-grayScaleBlack50 text-Caption1-sm font-semibold font-NotoSansKR line-clamp-1`}
-                      >
-                        예매일
-                      </p>
-                    </div>
-                    <p className="pt-6 text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR line-clamp-2">
-                      위켄드 내한공연 2026
-                    </p>
-                    <p className="pt-6 text-grayScaleBlack50 text-Body4-re font-regular font-NotoSansKR line-clamp-1">
-                      NOL 티켓
-                    </p>
-                  </div>
-                  <img
-                    src={CalenderInfoArrowIcon}
-                    className="ml-16 w-24 h-24"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col pt-16">
-                <div className="flex items-center">
-                  <div className="w-4 h-12 bg-lyricsOriginal rounded-1 mr-6" />
-                  <p className="text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR">
-                    18:00
-                  </p>
-                </div>
+              <ScheduleInfoItem
+                indicatorColor="bg-lyricsOriginal"
+                time="18:00"
+                badgeText="공연일"
+                title="위켄드 내한공연 2026인데 만약 내용이 길어지면 최대 두줄까지 표시합니다!!"
+                description="얘는 내용이 길어질 시 한 줄 까지만 표시합니다!!"
+              />
 
-                <div className="flex items-center justify-between bg-grayScaleBlack80 rounded-8 mt-10 px-12 py-12 cursor-pointer">
-                  <div>
-                    <div
-                      className={`inline-flex items-center justify-center rounded-24 bg-grayScaleBlack90 `}
-                    >
-                      <p
-                        className={`px-10 py-4 text-grayScaleBlack50 text-Caption1-sm font-semibold font-NotoSansKR line-clamp-1`}
-                      >
-                        공연일
-                      </p>
-                    </div>
-                    <p className="pt-6 text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR line-clamp-2">
-                      위켄드 내한공연 2026인데 만약 내용이 길어지면 최대
-                      두줄까지 표시합니다!!
-                    </p>
-                    <p className="pt-6 text-grayScaleBlack50 text-Body4-re font-regular font-NotoSansKR line-clamp-1">
-                      얘는 내용이 길어질 시 한 줄 까지만 표시합니다!!
-                    </p>
-                  </div>
-                  <img
-                    src={CalenderInfoArrowIcon}
-                    className="ml-16 w-24 h-24"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col pt-16">
-                <div className="flex items-center">
-                  <div className="w-4 h-12 bg-grayScaleBlack50 rounded-1 mr-6" />
-                  <p className="text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR">
-                    추후 발표
-                  </p>
-                </div>
+              <ScheduleInfoItem
+                indicatorColor="bg-grayScaleBlack50"
+                time="추후 발표"
+                badgeText="예매일"
+                title="요아소비 내한공연 2026"
+                description="추후 발표"
+              />
 
-                <div className="flex items-center justify-between bg-grayScaleBlack80 rounded-8 mt-10 px-12 py-12 cursor-pointer">
-                  <div>
-                    <div
-                      className={`inline-flex items-center justify-center rounded-24 bg-grayScaleBlack90 `}
-                    >
-                      <p
-                        className={`px-10 py-4 text-grayScaleBlack50 text-Caption1-sm font-semibold font-NotoSansKR line-clamp-1`}
-                      >
-                        예매일
-                      </p>
-                    </div>
-                    <p className="pt-6 text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR line-clamp-2">
-                      요아소비 내한공연 2026
-                    </p>
-                    <p className="pt-6 text-grayScaleBlack50 text-Body4-re font-regular font-NotoSansKR line-clamp-1">
-                      추후 발표
-                    </p>
-                  </div>
-                  <img
-                    src={CalenderInfoArrowIcon}
-                    className="ml-16 w-24 h-24"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col pt-16">
-                <div className="flex items-center">
-                  <div className="w-4 h-12 bg-grayScaleBlack50 rounded-1 mr-6" />
-                  <p className="text-grayScaleBlack5 text-Body4-md font-medium font-NotoSansKR">
-                    공연 취소
-                  </p>
-                </div>
-
-                <div className="flex items-center justify-between bg-grayScaleBlack80 rounded-8 mt-10 px-12 py-12 cursor-pointer">
-                  <div>
-                    <div
-                      className={`inline-flex items-center justify-center rounded-24 bg-grayScaleBlack90 `}
-                    >
-                      <p
-                        className={`px-10 py-4 text-grayScaleBlack50 text-Caption1-sm font-semibold font-NotoSansKR line-clamp-1`}
-                      >
-                        예매일
-                      </p>
-                    </div>
-                    <p className="pt-6 text-grayScaleWhite text-Body2-sm font-semibold font-NotoSansKR line-clamp-2">
-                      아도 내한공연 2026
-                    </p>
-                    <p className="pt-6 text-grayScaleBlack50 text-Body4-re font-regular font-NotoSansKR line-clamp-1">
-                      잠실 실내체육관
-                    </p>
-                  </div>
-                  <img
-                    src={CalenderInfoArrowIcon}
-                    className="ml-16 w-24 h-24"
-                  />
-                </div>
-              </div>
+              <ScheduleInfoItem
+                indicatorColor="bg-grayScaleBlack50"
+                time="공연 취소"
+                badgeText="예매일"
+                title="아도 내한공연 2026"
+                description="잠실 실내체육관"
+              />
             </div>
           </motion.div>
         </>
