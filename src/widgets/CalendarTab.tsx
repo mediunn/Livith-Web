e91faeac595a04257aa39e589ab3a5211aead72d
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-07-21 15:16:54 +0900

    feat: 캘린더 내 공연 로그인 모달 및 스케줄 에러 토스트 띄우기

--- src/widgets/CalendarTab.tsx
+++ src/widgets/CalendarTab.tsx
@@ -4,6 +4,11 @@ import { MonthlyCalendar } from "../features/calendar/ui/MonthlyCalendar";
 import { CalendarFilter } from "../features/calendar/ui/CalendarFilter";
 
 import { ConcertType, ScheduleType } from "../features/calendar/model/types";
+import { useRecoilValue } from "recoil";
+import { userState } from "../shared/lib/recoil/atoms/userState";
+import LoginModal from "../features/auth/ui/LoginModal";
+import { toast } from "react-toastify";
+import ErrorToast from "../shared/ui/Toast/ErrorToast";
 
 export default function CalendarTab() {
   const [scheduleTypes, setScheduleTypes] = useState<ScheduleType[]>([
@@ -12,20 +17,52 @@ export default function CalendarTab() {
   ]);
 
   const [concertType, setConcertType] = useState<ConcertType>(ConcertType.ALL);
+  const user = useRecoilValue(userState);
+  const [openLoginModal, setOpenLoginModal] = useState(false);
+  const handleConcertTypeChange = (value: ConcertType) => {
+    if (value === ConcertType.INTEREST && !user) {
+      setOpenLoginModal(true);
+      return;
+    }
 
+    setConcertType(value);
+  };
+  const handleScheduleTypesChange = (value: ScheduleType[]) => {
+    if (value.length === 0) {
+      toast(
+        <ErrorToast message={"예매일 또는 공연일 중 하나는 선택해야 해요."} />,
+        {
+          toastId: "calendar-schedule-type-error",
+          position: "top-center",
+          autoClose: 3000,
+          pauseOnFocusLoss: false,
+        },
+      );
+      return;
+    }
+
+    setScheduleTypes(value);
+  };
   return (
-    <div className="flex flex-col gap-4">
-      <CalendarFilter
-        scheduleTypes={scheduleTypes}
-        concertType={concertType}
-        onScheduleTypesChange={setScheduleTypes}
-        onConcertTypeChange={setConcertType}
-      />
+    <>
+      <div className="flex flex-col gap-4">
+        <CalendarFilter
+          scheduleTypes={scheduleTypes}
+          concertType={concertType}
+          onScheduleTypesChange={handleScheduleTypesChange}
+          onConcertTypeChange={handleConcertTypeChange}
+        />
 
-      <MonthlyCalendar
-        scheduleTypes={scheduleTypes}
-        concertType={concertType}
+        <MonthlyCalendar
+          scheduleTypes={scheduleTypes}
+          concertType={concertType}
+        />
+      </div>
+      <LoginModal
+        isOpen={openLoginModal}
+        onClose={() => setOpenLoginModal(false)}
+        type="interestConcert"
       />
-    </div>
+    </>
   );
 }
