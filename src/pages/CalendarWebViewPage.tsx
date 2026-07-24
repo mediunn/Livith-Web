b68c13b56417607f7bad7366a7ce38ad8a115cb0
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-07-24 23:23:25 +0900

    fix: 캘린더 날짜 선택 props 넘기도록 수정

--- src/pages/CalendarWebViewPage.tsx
+++ src/pages/CalendarWebViewPage.tsx
@@ -1,5 +1,6 @@
 import { MonthlyCalendar } from "../features/calendar/ui/MonthlyCalendar";
+import { sendSelectedDate } from "../shared/lib/webview/bridge";
 
 export default function CalendarWebViewPage() {
-  return <MonthlyCalendar />;
+  return <MonthlyCalendar onDateSelect={sendSelectedDate} />;
 }
